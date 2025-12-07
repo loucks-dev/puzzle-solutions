e990da93a06375f56bb65c66906d852bc6720c59
loucks-dev <parkerloucks74@gmail.com>
2025-12-06 22:39:22 -0600

    Finished part 1 of day 1

--- Advent/2025/Day 01/main.cpp
+++ Advent/2025/Day 01/main.cpp
@@ -1,6 +1,31 @@
 #include <fstream>
 #include <string>
+#include <iostream>
 
-int safeCracker(std::ifstream& inputFile, std::string& line){
+int safeCracker(std::ifstream& inputFile){
+    int ans = 0, dialCurrent = 50;
+    std::string line;
+    while (std::getline(inputFile, line)){
+        if (line[0] == 'R'){
+            dialCurrent = ((dialCurrent + std::stoi(line.substr(1)) % 100)+100) % 100;
+             if (dialCurrent == 0){
+                ans++;
+             }      
+        }
+        else if (line[0] == 'L'){
+            dialCurrent = ((dialCurrent - std::stoi(line.substr(1)) % 100) + 100) % 100;
+            if (dialCurrent == 0){
+                ans++;
+            }
+        }
+        std::cout << dialCurrent << std::endl;
+    }
+    return ans;
+}
+
+int main(){
+    std::ifstream file("data.txt");
+    std::cout << "ANSWER:" << safeCracker(file);
     
+    return 0;
 }
